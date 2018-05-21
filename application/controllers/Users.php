0acfe2aa08c755a1a05f5db77c8438356f94cd34
tzandev <tzandev@rainbowgrp.co.uk>
2018-05-21 18:23:33 +0300

    added error page

--- application/controllers/Users.php
+++ application/controllers/Users.php
@@ -8,18 +8,19 @@
 
 class Users extends CI_Controller{
 
+    private $message = '';
+
     public function __construct(){
 
         parent::__construct();
         $this->load->model('users_model');
         $this->load->library('form_validation');
+        $this->load->helper('form');
 
     }
 
     public function index(){
-
-        $data['title'] = "User profile";
-
+        $data['title'] = "All users";
         $this->load->view("header",$data);
         $this->load->view("profile");
         $this->load->view("footer");
@@ -27,23 +28,32 @@ class Users extends CI_Controller{
     }
 
     public function create_user(){
-        $data['title']="Create new user";
+        $data['title'] = "Create new user";
         $this->load->view('header',$data);
         $this->load->view('users/create');
         $this->load->view('footer');
     }
 
     public function create(){
-
         $this->users_model->create();
+    }
 
+    public function update_user(){
+        $data['title'] = "Update existing user";
+        $this->load->view('header',$data);
+        $this->load->view('users/create');
+        $this->load->view('footer');
     }
 
     public function update($id = NULL){
 
         if(empty($id)){
-            echo "Please provide a valid ID";
-            exit();
+            $data['message'] = "Please provide a valid ID";
+            $data['title'] = "Update existing user";
+            $this->load->view('header',$data);
+            $this->load->view('errors/error',$data);
+            $this->load->view('footer');
+            return;
         }
 
         $this->users_model->update($id);
---
+++ application/views/errors/error.php
@@ -0,0 +1,3 @@
+<div class="container">
+    <p><?php echo $message ?></p>
+</div>
